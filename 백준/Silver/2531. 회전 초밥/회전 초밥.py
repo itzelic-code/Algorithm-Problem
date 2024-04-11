67164003ae17512a27f82e77607cc04a8b262696
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2024-04-11 19:09:59 +0900

    [Silver I] Title: 회전 초밥, Time: 1308 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+n, d, k, c = map(int, input().split())
+sushi = [int(input()) for _ in range(n)]
+sushi += sushi
+
+kind = defaultdict(int)
+for i in range(k):
+    kind[sushi[i]] += 1
+
+result = len(kind)
+if c not in kind:  # 쿠폰 초밥이 윈도우에 없을 때
+    result += 1
+
+for i in range(k, len(sushi)):
+    kind[sushi[i]] += 1  # 새로운 초밥 추가
+    kind[sushi[i-k]] -= 1  # 윈도우 밖의 초밥 삭제
+    if kind[sushi[i-k]] == 0:
+        del kind[sushi[i-k]]  # 카운트가 0이면 삭제
+    
+    current = len(kind)
+    if c not in kind:  # 쿠폰 초밥이 윈도우에 없을 때
+        current += 1
+    result = max(result, current)  # 최대 종류 수 업데이트
+
+print(result)
