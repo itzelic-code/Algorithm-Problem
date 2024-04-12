266e90a8c35bacc7ea418448f2dbcee9f9e849ce
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2024-04-12 20:48:55 +0900

    [Silver III] Title: 1로 만들기, Time: 552 ms, Memory: 46748 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,9 @@
+N = int(input())
+dp = [0, 0, 1, 1] + [0] * (N-3)
+for i in range(4, N+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+print(dp[N])
