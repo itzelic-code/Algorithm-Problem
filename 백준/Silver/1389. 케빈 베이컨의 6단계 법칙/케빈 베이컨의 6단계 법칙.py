31a7693794469ec6847b8df55388dcc79583cfc7
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2024-04-16 00:33:12 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 460 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+graph = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i == j:
+                graph[i][j] = 0
+            else:
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+result = [sum(row[1:]) for row in graph[1:]]
+print(result.index(min(result))+1)
