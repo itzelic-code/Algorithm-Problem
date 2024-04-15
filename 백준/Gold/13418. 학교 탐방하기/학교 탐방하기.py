709dc108933a68263fd4e82447508135564750d7
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2024-04-15 17:09:59 +0900

    [Gold III] Title: 학교 탐방하기, Time: 684 ms, Memory: 90484 KB -BaekjoonHub

--- 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.py
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.py
@@ -1,11 +1,11 @@
 from sys import stdin
+input = stdin.readline
 
 def solution(N,edges):
 
     worstCourse, bestCourse = 0, N
     maxParent, minParent = [i for i in range(N+1)], [i for i in range(N+1)]
-
-    # [1] Union & Find
+    
     def union(n1, n2, parent):
         n1 = find(n1, parent)
         n2 = find(n2, parent)
@@ -15,16 +15,15 @@ def solution(N,edges):
             parent[n] = find(parent[n],parent)
         return parent[n]
     
-    # [2] 최적, 최악의 코스 찾기
     for n1, n2, w in edges:
-        # [2-1] 최적의 코스
+        # 최적의 코스
         if w == 1:
             n1 = find(n1,minParent)
             n2 = find(n2,minParent)
             if n1 != n2:
                 bestCourse -= 1
                 union(n1, n2, minParent)
-        # [2-2] 최악의 코스
+        # 최악의 코스
         else:
             n1 = find(n1,maxParent)
             n2 = find(n2,maxParent)
@@ -34,10 +33,7 @@ def solution(N,edges):
 
     return worstCourse**2 - bestCourse**2
 
-# input
-N, M = map(int,stdin.readline().split())
-edges = [tuple(map(int,stdin.readline().split())) for _ in range(M+1)]
-
-# result
+N, M = map(int,input().split())
+edges = [tuple(map(int,input().split())) for _ in range(M+1)]
 result = solution(N,edges)
 print(result)
