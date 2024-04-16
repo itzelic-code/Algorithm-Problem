bb9a407fc1c82af18b13bfbc311328b9c6984c6c
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2024-04-16 13:01:44 +0900

    [Silver I] Title: 경로 찾기, Time: 212 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,11 @@
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+for row in graph:
+    print(' '.join(map(str, row)))
