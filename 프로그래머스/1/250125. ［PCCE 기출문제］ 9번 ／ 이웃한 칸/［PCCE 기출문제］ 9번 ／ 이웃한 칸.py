4c5383b9b420c3fe0e07233e7c0120eb6728883e
Itzell Kim <121436066+itzelic-code@users.noreply.github.com>
2024-07-03 02:38:30 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,12 +1,10 @@
 def solution(board, h, w):
-    cnt = 0
-    n = len(board)
-    c = board[h][w]
-    dh = [0, 1, -1, 0]
-    dw = [1, 0, 0, -1]
+    cnt, n = 0, len(board)
+    dh, dw = [0, 1, -1, 0], [1, 0, 0, -1]
     for i in range(4):
         h_check = h + dh[i]
         w_check = w + dw[i]
-        if 0 <= h_check < n and 0 <= w_check < n and c == board[h_check][w_check]:
+        if 0 <= h_check < n and 0 <= w_check < n and board[h][w] == board[h_check][
+            w_check]:
             cnt += 1
     return cnt
