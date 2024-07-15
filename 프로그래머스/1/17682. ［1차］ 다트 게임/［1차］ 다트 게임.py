9b666f7d022d7bbbc83902fc162077c86aa29ace
Itzell Kim <121436066+itzelic-code@users.noreply.github.com>
2024-07-15 21:56:47 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,32 +1,3 @@
 def solution(dartResult):
-    n = ''  # 현재 숫자를 저장하는 변수
-    score = []  # 각 라운드의 점수를 저장하는 리스트
-    for i in dartResult:
-        # 숫자인 경우
-        if i.isnumeric():
-            n += i  # 현재 숫자에 추가
-        # S, D, T 인 경우
-        elif i == 'S':
-            n = int(n) ** 1  # 현재 숫자의 1제곱
-            score.append(n)  # 점수 리스트에 추가
-            n = ''  # 현재 숫자 초기화
-        elif i == 'D':
-            n = int(n) ** 2  # 현재 숫자의 2제곱
-            score.append(n)  # 점수 리스트에 추가
-            n = ''  # 현재 숫자 초기화
-        elif i == 'T':
-            n = int(n) ** 3  # 현재 숫자의 3제곱
-            score.append(n)  # 점수 리스트에 추가
-            n = ''  # 현재 숫자 초기화
-        # * 인 경우
-        elif i == '*':
-            if len(score) > 1:
-                score[-2] = score[-2] * 2  # 이전 라운드 점수 2배
-                score[-1] = score[-1] * 2  # 현재 라운드 점수 2배
-            else:
-                score[-1] = score[-1] * 2  # 현재 라운드 점수 2배
-        # # 인 경우
-        elif i == '#':
-            score[-1] = score[-1] * -1  # 현재 라운드 점수 -1배
-    
-    return sum(score)  # 모든 라운드의 점수 합 반환
+    answer = 0
+    return answer
