fc3b715d18fd124afe4215aafaa133367251cd81
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2025-01-17 15:34:44 +0900

    [Bronze II] Title: 피로도, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.py
+++ 백준/Bronze/22864. 피로도/피로도.py
@@ -1,19 +1,16 @@
 import sys
-input = sys.stdin.readline
 
-A, B, C, M = map(int, input().split())
+A, B, C, M = map(int, sys.stdin.readline().strip().split())
 tired = 0
 work = 0
 
-for _ in range(24):
-    if M < A:
-        break
-    
-    if tired + A <= M:
-        tired += A
-        work += B
-    else:
-        tired -= C
-        if tired < 0:
-            tired = 0
-print(work)
+if M < A:
+    print(0)
+else:
+    for _ in range(24):
+        if tired + A <= M:
+            tired += A
+            work += B
+        else:
+            tired = max(0, tired - C)
+    print(work)
