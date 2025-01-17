45a2d3597c54b69b5db392d7f3b77ca5dac68256
Sohyun Kim <121436066+itzelic-code@users.noreply.github.com>
2025-01-17 14:37:10 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,7 +1,12 @@
 def is_prime(num):
     if num <= 1:
         return False
-    for i in range(2, num):
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    sqrt_num = int(num ** 0.5)
+    for i in range(3, sqrt_num + 1, 2):
         if num % i == 0:
             return False
     return True
